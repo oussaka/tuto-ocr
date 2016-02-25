c324e2cb234a5ffb0148d050528685a5579b68a5
cthubert <cthubert@PC-LUS-13731406>
2016-02-25 14:20:36 +0100

    Définition des routes de notres platformes, AVEC préfixe dans app/config/routing.yml Pour chaque route, une action par défaut est ajoutée dans le contrôleur (le cas échéant) Routes (A=Ajouter, S=Supprimer) : S	/hello-world A	/platform/{page} A	/platform/advert/{id} A	/platform/add A	/platform/edit/{id} A	/platform/delete/{id}

--- src/ChrisScientistPlatformBundle/Controller/AdvertController.php
+++ src/ChrisScientistPlatformBundle/Controller/AdvertController.php
@@ -19,4 +19,19 @@ class AdvertController extends Controller
         // en récupérant un paramètre de la route ($id)
         return new Response("Affichage de l'annonce dont l'ID est &laquo; ".$id." &raquo;.") ;
     }
+    
+    public function addAction()
+    {
+        return new Response("Pour ajouter une annonce, revenez plus tard...") ;
+    }
+    
+    public function editAction($id)
+    {
+        return new Response("Pour modifier l'annonce ".$id.", revenez plus tard...") ;
+    }
+    
+    public function deleteAction($id)
+    {
+        return new Response("Pour supprimer l'annonce ".$id.", revenez plus tard...") ;
+    }
 }
