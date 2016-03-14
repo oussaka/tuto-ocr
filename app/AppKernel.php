19b766fefb0b8ffae22b6e2a5ef63898e30504c1
cthubert <cthubert@PC-LUS-13731406>
2016-03-14 17:24:11 +0100

    [Sécurité] Utilisation du bundle FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new ChrisScientistPlatformBundle\ChrisScientistPlatformBundle(),
             new ChrisScientistUserBundle\ChrisScientistUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
--- src/ChrisScientistUserBundle/Entity/Utilisateur.php
+++ src/ChrisScientistUserBundle/Entity/Utilisateur.php
@@ -3,15 +3,14 @@
 namespace ChrisScientistUserBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface ;
+use FOS\UserBundle\Model\User as BaseUser ;
 
 /**
  * Utilisateur
  *
- * @ORM\Table(name="utilisateur")
- * @ORM\Entity(repositoryClass="ChrisScientistUserBundle\Repository\UtilisateurRepository")
+ * @ORM\Entity
  */
-class Utilisateur implements UserInterface
+class Utilisateur extends BaseUser
 {
     /**
      * @var int
@@ -20,146 +19,6 @@ class Utilisateur implements UserInterface
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="username", type="string", length=255, unique=true)
-     */
-    private $username;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="password", type="string", length=255)
-     */
-    private $password;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="salt", type="string", length=255)
-     */
-    private $salt;
-
-    /**
-     * @var array
-     *
-     * @ORM\Column(name="roles", type="array")
-     */
-    private $roles;
-
-    public function __construct()
-    {
-        $this->roles = array() ;
-    }
-    
-
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set username
-     *
-     * @param string $username
-     * @return Utilisateur
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    /**
-     * Get username
-     *
-     * @return string 
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    /**
-     * Set password
-     *
-     * @param string $password
-     * @return Utilisateur
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * Get password
-     *
-     * @return string 
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    /**
-     * Set salt
-     *
-     * @param string $salt
-     * @return Utilisateur
-     */
-    public function setSalt($salt)
-    {
-        $this->salt = $salt;
-
-        return $this;
-    }
-
-    /**
-     * Get salt
-     *
-     * @return string 
-     */
-    public function getSalt()
-    {
-        return $this->salt;
-    }
-
-    /**
-     * Set roles
-     *
-     * @param array $roles
-     * @return Utilisateur
-     */
-    public function setRoles($roles)
-    {
-        $this->roles = $roles;
-
-        return $this;
-    }
-
-    /**
-     * Get roles
-     *
-     * @return array 
-     */
-    public function getRoles()
-    {
-        return $this->roles;
-    }
-    
-    public function eraseCredentials()
-    {
-        
-    }
 }
